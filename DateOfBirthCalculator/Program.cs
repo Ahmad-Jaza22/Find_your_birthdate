c83dd576d5f6736af1b70391abac030932131cbe
Ahmad <ahmad.jazaa22@gmail.com>
2021-01-13 16:41:37 +0300

    this is simple algorithm that tells a persons birth year using 2021

---
+++ DateOfBirthCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DateOfBirthCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+
+            Console.WriteLine("enter your age \n");
+
+            string age = Console.ReadLine();
+
+            DateTime now = DateTime.Now;
+            
+
+            int birth = 2021 - int.Parse(age);
+
+            Console.WriteLine("\n your date of birth is: " + birth);
+
+            
+        }
+    }
+}
